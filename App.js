16498d4160aa2362ab2e1e319116dc9b976b8a16
moundir99 <moundirboucherab@gmail.com>
2020-10-26 15:37:19 +0100

    commit osuf

--- App.js
+++ App.js
@@ -2,24 +2,23 @@
 import Navigationbar from './component/Navigationbar';
 import Header from './component/Header';
 import React from 'react';
-import {Text, View,StyleSheet} from 'react-native';
-import {StatusBar} from "expo-status-bar";
+import { Text, View, StyleSheet } from 'react-native';
+import { StatusBar } from "expo-status-bar";
 
 
 
-  export default function App() {
-    return (
-        <View style={style.container}>
-          <Navigationbar/>
-        </View>
-      )
-  }
-
-  const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-    },
+export default function App() {
+  return (
+    <View style={style.container}>
+      <Navigationbar />
+    </View>
+  )
+}
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
 });
 
